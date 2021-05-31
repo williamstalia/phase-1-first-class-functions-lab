7a2073d49bcbba4a360cdc0f1d60a2046daf45e9
Talia Williams <williams.talia101@gmail.com>
2021-05-31 05:50:04 -0500

    commit tree 8e41a3929b4d6190595966b2994e37c22f3ebe37

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (driversArray) {
+    return driversArray.slice(0, 2)
+}
+const returnLastTwoDrivers = function (driversArray) {
+    return driversArray.slice(2, 5)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+        return function(multiplier) {
+           return (multiplier * 4)
+        }
+    }
+    
+const fareDoubler = fare => fare * 2;
+
+const fareTripler = fare => fare * 3;
+
+const selectDifferentDrivers = function(drivers, eitherDriver) {
+    return eitherDriver(drivers)
+}
